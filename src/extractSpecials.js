c89de1bdc7e4c53692d2b2969b427e5e7ec964e9
DrAVl <DrAV432@mail.ru>
2024-09-14 01:08:05 +0700

    1 commit

---
+++ src/extractSpecials.js
@@ -0,0 +1,7 @@
+function extractSpecials({ special }) {
+    return special.map(({ id, name, icon, description = 'Описание недоступно' }) => ({
+      id, name, icon, description
+    }));
+  }
+  
+  export default extractSpecials;
---
+++ src/extractSpecials.test.js
@@ -0,0 +1,58 @@
+import extractSpecials from './extractSpecials';
+
+test('should extract specials with all properties', () => {
+  const character = {
+    name: 'Лучник',
+    type: 'Bowman',
+    health: 50,
+    level: 3,
+    attack: 40,
+    defence: 10,
+    special: [
+      {
+        id: 8,
+        name: 'Двойной выстрел',
+        icon: 'http://...',
+        description: 'Двойной выстрел наносит двойной урон'
+      }
+    ]
+  };
+
+  const result = extractSpecials(character);
+  expect(result).toEqual([
+    {
+      id: 8,
+      name: 'Двойной выстрел',
+      icon: 'http://...',
+      description: 'Двойной выстрел наносит двойной урон'
+    }
+  ]);
+});
+
+test('should set default description if not provided', () => {
+  const character = {
+    name: 'Лучник',
+    type: 'Bowman',
+    health: 50,
+    level: 3,
+    attack: 40,
+    defence: 10,
+    special: [
+      {
+        id: 9,
+        name: 'Нокаутирующий удар',
+        icon: 'http://...'
+      }
+    ]
+  };
+
+  const result = extractSpecials(character);
+  expect(result).toEqual([
+    {
+      id: 9,
+      name: 'Нокаутирующий удар',
+      icon: 'http://...',
+      description: 'Описание недоступно'
+    }
+  ]);
+});
